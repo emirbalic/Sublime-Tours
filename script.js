509ca17ede1296b7abaeedaf7e131b5f767c0cb6
Emir Balic <emirbalic@gmail.com>
2019-01-07 17:57:51 +0100

    fill exact data, style page

--- script.js
+++ script.js
@@ -15,11 +15,15 @@ $(document).ready(function () {
 
 
     if (position >= 100) {
+      $('logo4, img').attr('style', 'color: white');
       $('nav-links, a').attr('style', 'color: white');
-      $('nav, nav-container').addClass('navBackground')
+      $('nav, nav-container').addClass('navBackground');
+      $('nav-icons, path').attr('style', 'color: white');
     } else {
+      $('logo4, img').attr('style', 'color: #03243e');
       $('nav-links, a').attr('style', 'color: #03243e');
       $('nav, nav-container').removeClass('navBackground')
+      $('nav-icons, path').attr('style', 'color: #03243e');
 
     }
 
